f4671118bc777ad963e0eecad6c79185c58a8f21
gr8kev <ebukakels@gmail.com>
2023-10-19 06:10:11 +0100

    react

--- src/App.js
+++ src/App.js
@@ -1,10 +1,20 @@
 
 import './App.css';
+import Header from './components/Header';
+import Balance from './components/Balance';
+import IncomeExpense from './components/IncomeExpense';
+import TransactionList from './components/TransactionList';
+import AddTransaction from './AddTransaction';
 
 function App() {
   return (
     <div>
-      hello world
+      <Header />
+      <div className='container'></div>
+      <Balance />
+      <IncomeExpense />
+      <TransactionList />
+      <AddTransaction/>
     </div>
   );
 }
