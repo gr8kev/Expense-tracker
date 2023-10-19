66f93164d847d907ee1169990b6ae29feb3bf64c
gr8kev <ebukakels@gmail.com>
2023-10-19 06:42:16 +0100

    react

--- src/AddTransaction.js
+++ src/AddTransaction.js
@@ -15,8 +15,8 @@ const AddTransaction = () => {
         <div className="form-control">
           <label htmlFor="amount"
             >Amount <br />
-            (negative - expense, positive - income)</label
-          >
+                      (negative - expense, positive - income)
+                  </label>
           <input type="number" value={amount} onChange={(e) => setAmount(e.target.value)} placeholder="Enter amount..." />
         </div>
         <button className="btn">Add transaction</button>
